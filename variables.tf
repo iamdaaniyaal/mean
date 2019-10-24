f3405cf93030846ea6ada76e49a2fb32455a7072
Mohammed Daaniyaal <35423314+iamdaaniyaal@users.noreply.github.com>
2019-10-24 19:33:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "credentials" {}
 variable "gcp_project" {}
 variable "region" {}
+variable "mean_stack_instance_ip_name" {}
 variable "mean_stack_instance_ip_region" {}
 variable "mean_stack_instance_name" {}
 variable "mean_stack_machine_type" {}
